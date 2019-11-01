e5b4d367c6b8a0734e2679716cf981f631023b1c
Ruchi Pandey <pandeyruchi.30@gmail.com>
2019-11-01 16:37:32 +0800

    ReverseString

--- src/main/java/com/ruchi/leetcode/ReverseStringII.java
+++ src/main/java/com/ruchi/leetcode/ReverseStringII.java
@@ -3,7 +3,22 @@ package com.ruchi.leetcode;
 public class ReverseStringII {
     public String reverseStr(String s, int k) {
 
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < s.length() - 1; i = i + k + k) {
+            str.append(getReverse(s.substring(i, i + k)));
+            System.out.println(str);
+        }
         return s;
+    }
+
+    private String getReverse(String s) {
+        char[] chars = s.toCharArray();
+        for (int i = 0, j = chars.length - 1; i < j; i++, j--) {
+            char temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+        }
+        return String.valueOf(chars);
     }
 
 }
