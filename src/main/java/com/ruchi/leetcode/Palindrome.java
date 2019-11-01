5e6f51be54a043b33a16e93a18cac1ad52c67e04
Ruchi Pandey <pandeyruchi.30@gmail.com>
2019-11-01 15:28:41 +0800

    Incorporated changes

--- src/main/java/com/ruchi/leetcode/Palindrome.java
+++ src/main/java/com/ruchi/leetcode/Palindrome.java
@@ -5,20 +5,14 @@ import javax.sound.midi.Soundbank;
 public class Palindrome {
     public Boolean checkPalindrome(String str)
     {
-        Boolean result = true;
         char[] chars = str.toCharArray();
         for(int i=0,j=chars.length-1;i<j;i++,j--)
         {
-          if(chars[i]==chars[j])
+          if(chars[i]!=chars[j])
           {
-              result =true;
-          }
-          else
-          {
-              result =false;
-              return result;
+return false;
           }
         }
-        return result;
+        return true;
     }
 }
