0835dd26e962a9e7b8f1df6de668341a52616ff1
Ruchi Pandey <pandeyruchi.30@gmail.com>
2019-11-02 23:23:47 +0800

    Two ways to reverse string

--- src/main/java/com/ruchi/leetcode/StringReverse.java
+++ src/main/java/com/ruchi/leetcode/StringReverse.java
@@ -4,11 +4,20 @@ public class StringReverse {
     public String reverse(String str) {
         char[] chars = str.toCharArray();
         for (int i = 0, j = chars.length - 1; i < j; i++, j--) {
-            char temp = chars[i];
-            chars[i] = chars[j];
-            chars[j] = temp;
+            chars[i]= (char)(chars[i]^chars[j]);
+            chars[j] = (char)(chars[i]^chars[j]);;
+            chars[i] = (char)(chars[i]^chars[j]);;
         }
         return String.valueOf(chars);
+    }
+
+    public char[] reverseString(char[] s) {
+        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+            char temp = s[i];
+            s[i]=s[j];
+            s[j]=temp;
+        }
+        return s;
     }
 }
 
--- src/test/java/com/ruchi/leetcode/StringReverseTest.java
+++ src/test/java/com/ruchi/leetcode/StringReverseTest.java
@@ -7,10 +7,13 @@ import static org.junit.Assert.*;
 public class StringReverseTest {
 
     StringReverse obj = new StringReverse();
+
     @Test
     public void reverse() {
-        assertEquals("abcdefg",obj.reverse("gfedcba"));
-        assertEquals("ZAAAabcdefg",obj.reverse("gfedcbaAAAZ"));
-        assertEquals("abcdefg%iop",obj.reverse("poi%gfedcba"));
+        assertEquals("abcdefg", obj.reverse("gfedcba"));
+        assertEquals("ZAAAabcdefg", obj.reverse("gfedcbaAAAZ"));
+        assertEquals("abcdefg%iop", obj.reverse("poi%gfedcba"));
+        assertArrayEquals(new char[]{'o', 'l', 'l', 'e', 'h'}, obj.reverseString(new char[]{'h', 'e', 'l', 'l', 'o'}));
     }
 }
+
