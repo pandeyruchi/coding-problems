946f5016fda785c8934696cbf3659b9f1bdbf60c
Ruchi Pandey <pandeyruchi.30@gmail.com>
2019-11-03 21:22:30 +0800

    balanced string

--- src/main/java/com/ruchi/leetcode/BalancedString.java
+++ src/main/java/com/ruchi/leetcode/BalancedString.java
@@ -2,7 +2,22 @@ package com.ruchi.leetcode;
 
 public class BalancedString {
     public int balancedStringSplit(String s) {
-        s.toCharArray()
-        return 0;
+        char[] chars = s.toCharArray();
+        int count=0,newCount =0;
+        for (int i = 0; i < chars.length; i++) {
+           if(chars[i]=='R')
+           {
+               count++;
+           }
+           else
+           {
+               count--;
+           }
+           if(count==0)
+           {
+               newCount++;
+           }
+        }
+        return newCount;
     }
 }
