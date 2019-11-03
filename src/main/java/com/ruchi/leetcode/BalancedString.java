12b58337a2c36199700769ed2bdbd1b2859f8ed5
Ruchi Pandey <pandeyruchi.30@gmail.com>
2019-11-03 21:25:24 +0800

    change variable name

--- src/main/java/com/ruchi/leetcode/BalancedString.java
+++ src/main/java/com/ruchi/leetcode/BalancedString.java
@@ -3,7 +3,7 @@ package com.ruchi.leetcode;
 public class BalancedString {
     public int balancedStringSplit(String s) {
         char[] chars = s.toCharArray();
-        int count=0,newCount =0;
+        int count=0,balancedStingCount =0;
         for (int i = 0; i < chars.length; i++) {
            if(chars[i]=='R')
            {
@@ -15,9 +15,9 @@ public class BalancedString {
            }
            if(count==0)
            {
-               newCount++;
+               balancedStingCount++;
            }
         }
-        return newCount;
+        return balancedStingCount;
     }
 }
