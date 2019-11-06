1248f823e69e86cb5c7daa61fa5493a931de599e
Ruchi Pandey <pandeyruchi.30@gmail.com>
2019-11-06 12:00:27 +0800

    string compression

--- src/main/java/com/ruchi/leetcode/StringCompression.java
+++ src/main/java/com/ruchi/leetcode/StringCompression.java
@@ -58,23 +58,7 @@ import java.util.List;
 //All characters have an ASCII value in [35, 126].
 //1 <= len(chars) <= 1000.
 public class StringCompression {
-    public int compress(char[] chars) {
-        ///int[] array = new int[256];
-        /// for (int i = 0; i < chars.length; i++) {
-        //            array[chars[i]] += 1;
-        //        }
-        //        int result = 0;
-        //        for (int i = 0; i < array.length; i++) {
-        //            int count = array[i];
-        //            if(count == 0){
-        //                continue;
-        //            }
-        //            else if (count == 1) {
-        //                result += 1;
-        //            } else {
-        //                result += (count + "").length() + 1;
-        //            }
-        //        }
+    public int compress1(char[] chars) {
         int count = 0;
         char temp = chars[0];
         int old = 0;
@@ -110,24 +94,115 @@ public class StringCompression {
         return newStr.length();
     }
 
-    private void pushZerosToEnd(char[] chars) {
-        int count = 0;  // Count of non-zero elements
 
-        // Traverse the array. If element encountered is
-        // non-zero, then replace the element at index 'count'
-        // with this element
-        for (int i = 0; i < chars.length; i++)
-            if (chars[i] != '!')
-                chars[count++] = chars[i]; // here count is
-        // incremented
-
-        // Now all non-zero elements have been shifted to
-        // front and 'count' is set as index of first 0.
-        // Make all elements 0 from count to end.
-        while (count < chars.length)
-            chars[count++] = '!';
+    public int compress(char[] chars) {
+        int finalLength = 0;
+        for (int i = 0; i < chars.length; ) {
+            char current = chars[i];
+            int count = 1;
+            int j = i + 1;
+            while ( j < chars.length && chars[j] == current) {
+                count++;
+                chars[j] = '!';
+                j++;
+            }
+            if (count > 1) {
+                char[] countAsString = String.valueOf(count).toCharArray();
+                for (int k = 0, l = i + 1; k < countAsString.length; k++, l++) {
+                    chars[l] = countAsString[k];
+                    finalLength++;
+                }
+            }
+            i = i + count;
+            finalLength++;
+        }
+        pushZerosToEnd(chars);
+        return finalLength;
     }
 
 
+    private void pushZerosToEnd(char[] chars) {
+        int count = 0;  // Count of non-zero elements
+
+        for (int i = 0; i < chars.length; i++)
+            if (chars[i] != '!')
+                chars[count++] = chars[i]; // here count is
+
+        while (count < chars.length)
+            chars[count++] = '!';
+    }
 }
 
+
+/// int count = 0;
+//        char temp = chars[0];
+//        int old = 0;
+//
+//        for (int i = 0; i < chars.length; i++) {
+//            {
+//                if (chars[i] == temp) {
+//                    count++;
+//                    if (count == 2) {
+//                        old = i;
+//                        chars[i] = (char) (count + '0');
+//                    } else if (count > 2 && count < 10) {
+//                        chars[old] = (char) (count + '0');
+//                        chars[i] = '!';
+//                    } else if (count >= 10) {
+//                        String number = String.valueOf(count);
+//                        char[] digits1 = number.toCharArray();
+//                        chars[old] = digits1[0];
+//                        for (int j = 1; j < digits1.length; j++) {
+//                            chars[old + 1] = digits1[j];
+//                        }
+//                        chars[i] = '!';
+//                    }
+//                } else {
+//                    temp = chars[i];
+//                    count = 1;
+//                }
+//            }
+//        }
+//        String s = String.valueOf(chars);
+//        String newStr = s.replaceAll("!", "");
+//        pushZerosToEnd(chars);
+//        return newStr.length();
+//    }
+//
+//    private void pushZerosToEnd(char[] chars) {
+//        int count = 0;  // Count of non-zero elements
+//
+//        // Traverse the array. If element encountered is
+//        // non-zero, then replace the element at index 'count'
+//        // with this element
+//        for (int i = 0; i < chars.length; i++)
+//            if (chars[i] != '!')
+//                chars[count++] = chars[i]; // here count is
+//        // incremented
+//
+//        // Now all non-zero elements have been shifted to
+//        // front and 'count' is set as index of first 0.
+//        // Make all elements 0 from count to end.
+//        while (count < chars.length)
+//            chars[count++] = '!';
+//    }
+
+
+///*****************************************
+///   int[] array = new int[256];
+//        for (int i = 0; i < chars.length; i++) {
+//            array[chars[i]] += 1;
+//        }
+//        int result = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            int count = array[i];
+//            if (count == 0) {
+//                continue;
+//            } else if (count == 1) {
+//                result += 1;
+//            } else {
+//                result += (count + "").length() + 1;
+//            }
+//        }
+//
+//        return result;
--- src/test/java/com/ruchi/leetcode/StringCompressionTest.java
+++ src/test/java/com/ruchi/leetcode/StringCompressionTest.java
@@ -6,14 +6,15 @@ import static org.junit.Assert.*;
 
 public class StringCompressionTest {
     StringCompression obj = new StringCompression();
+
     @Test
     public void compress() {
-      ///  assertEquals(6,obj.compress(new char[]{'a','a','b','b','c','c','c'}));
-        //assertEquals(1,obj.compress(new char[]{'a'}));
-        //assertEquals(4,obj.compress(new char[]{'a','b','b','b','b','b','b','b','b','b','b','b','b'}));
-        assertEquals(6,obj.compress(new char[]{'a','a','a','b','b','a','a'}));
-        assertEquals(6,obj.compress(new char[]{'a','a','a','b','b','b','c','c','c'}));
-        assertEquals(3,obj.compress(new char[]{'a','a','a','a','a','b'}));
-        assertEquals(3,obj.compress(new char[]{'o','o','o','o','o','o','o','o','o','o'}));
+        assertEquals(6, obj.compress(new char[]{'a', 'a', 'b', 'b', 'c', 'c', 'c'}));
+        assertEquals(1, obj.compress(new char[]{'a'}));
+        assertEquals(4, obj.compress(new char[]{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'}));
+        assertEquals(6, obj.compress(new char[]{'a', 'a', 'a', 'b', 'b', 'a', 'a'}));
+        assertEquals(6, obj.compress(new char[]{'a', 'a', 'a', 'b', 'b', 'b', 'c', 'c', 'c'}));
+        assertEquals(3, obj.compress(new char[]{'a', 'a', 'a', 'a', 'a', 'b'}));
+        assertEquals(3, obj.compress(new char[]{'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'}));
     }
 }
