075d277002b61a16202e9512f0c962fcad54a539
Ruchi Pandey <pandeyruchi.30@gmail.com>
2019-11-08 23:41:42 +0800

    Initial Code

---
+++ src/test/java/com/ruchi/leetcode/RobotReturnOriginTest.java
@@ -0,0 +1,14 @@
+package com.ruchi.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RobotReturnOriginTest {
+    RobotReturnOrigin obj = new RobotReturnOrigin();
+    @Test
+    public void judgeCircle() {
+        assertEquals(true,obj.judgeCircle("UD"));
+        assertEquals(false,obj.judgeCircle("LL"));
+    }
+}
